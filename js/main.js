8ea2a78770dc8ce03b6ebf85dbce49c51ab2e994
gdapriana <gedeapriana36@gmail.com>
2026-07-06 15:20:40 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  $("#highlight .highlight-bg-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    cssEase: "linear",
+    arrows: false,
+    dots: false,
+    infinite: true,
+    speed: 1000,
+  });
+});
